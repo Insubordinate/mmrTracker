2fd366e2124313a78d6f20a9a9618a5311d152b4
insubordinate <axl.cruz8344@gmail.com>
2017-04-22 16:48:09 -0400

    Finished the functions, need to make them pretty

--- mmr.py
+++ mmr.py
@@ -22,15 +22,43 @@ def addChampion():
     
 def addMMR():
     champion = raw_input("Please enter your champion:")
-    addToMMR = raw_input("How much did you change?")
+    newMMR = str(raw_input("What is your new MMR: "))
 
-    info = json.load()
-    print  info['overallMMR']
+    with open('data.JSON',"r") as i:
+        info = json.load(i)
+        counter = 0
+        index = 0
+        for x in info['champions']:
+            for k in x:
+                if k == champion:
+                    index = counter
+                else:
+                    counter+=1
 
-def addToMmmr(file):
-    return 0
+        x = int(newMMR) - int(info['champions'][index][champion]['currentMMR'])
+        print "change is " + str(x)    
+        info['champions'][index][champion]['currentMMR'] = newMMR
+        
+    with open('data.JSON','w') as o:
+              json.dump(info,o)
+
+        
+
+                    
+                    
+                    
+            
 
 
+def addToMmr():
+    newMMR = str(raw_input("What is your new MMR: "))
+
+    with open('data.JSON',"r") as i:
+        info = json.load(i)
+        info['overallMMR'] = newMMR
+    
+    with open('data.JSON','w') as o:
+              json.dump(info,o)
 
 
 
@@ -38,12 +66,17 @@ def addToMmmr(file):
 
 while True:
     print "What would you like to do"
+
     print "1 : Add a champion"
     print "2 : Add MMR to a champion"
     print "3 : Add to your overall MMR"
     print "4 : Graph your MMR"
     print "5 : Graph a champion's MMR"
-    print "6 : Quit the program"
+    print "6 : Print your current MMR"
+    print "7 : Print champion MMR"
+    
+    
+    print "8 : Quit the program"
 
     option = int(raw_input("Enter a number "))
 
@@ -51,7 +84,10 @@ while True:
         addChampion()
 
     elif option == 2:
-        addMMR(f)        
+        addMMR()
+    elif option ==3:
+        addToMmr()
 
-    if option == 6:
+    
+    if option == 8:
          sys.exit()
